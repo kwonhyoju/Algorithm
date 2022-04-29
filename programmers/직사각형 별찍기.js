5802b75d9d8f4abeee81d99866e75e8b41f3f34b
noah.kwon <saung3754@hanmail.net>
2022-04-29 17:10:20 +0900

    직사각형 별찍기

---
+++ programmers/직사각형 별찍기.js
@@ -0,0 +1,14 @@
+process.stdin.setEncoding("utf8");
+process.stdin.on("data", (data) => {
+    const n = data.split(" ");
+    const a = Number(n[0]),
+        b = Number(n[1]);
+
+    for (let i = 0; i < b; i++) {
+        let text = "";
+        for (let j = 1; j <= a; j++) {
+            text += "*";
+        }
+        console.log(text);
+    }
+});
