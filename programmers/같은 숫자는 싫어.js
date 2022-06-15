33fb8fccd25e3681fb6a33f40babd18faae9ed72
noah.kwon <saung3754@hanmail.net>
2022-06-15 14:26:10 +0900

    같은 숫자는 싫어

---
+++ programmers/같은 숫자는 싫어.js
@@ -0,0 +1,8 @@
+function solution(arr) {
+    let answer = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== arr[i + 1]) answer.push(arr[i]);
+    }
+    return answer;
+    //return arr.filter((val,index) => val != arr[index+1]);
+}
